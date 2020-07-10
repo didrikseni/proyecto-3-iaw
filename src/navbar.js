e0c74c5d6ac6d620ed370f0a8a1ac9fedbb4b645
Ian Didriksen <iandidri@gmail.com>
2020-07-10 15:04:45 -0300

    Changes in components

--- src/navbar.js
+++ src/navbar.js
@@ -3,16 +3,56 @@ import './css/navbar.css';
 
 function Item(props) {
     return (
-        <button className="nabvar-item nav-pills nabvar"> { props.name } </button>
+        <a className="nabvar-item nav-pills nabvar custom-button"> { props.name } </a>
+    );
+}
+
+function NavbarList(props) {
+    if (props.isLoggedIn)
+        return (
+        <ul className="navbar-nav nav-pills nav-justified">
+            <li className="nav-item">
+                <div className="nabvar-item nav-pills nabvar">
+                    <h5>Articulo</h5>
+                </div>
+            </li>
+            <li className="mx-2 nav-item">
+                <div className="nabvar-item nav-pills nabvar">
+                    <h5>{props.name}</h5>
+                </div>
+            </li>
+            <li className="mx-2 nav-item">
+                <div className="nabvar-item nav-pills nabvar">
+                    <h5>Salir</h5>
+                </div>
+            </li>
+        </ul>
+    );
+    else return (
+        <ul className="navbar-nav nav-pills nav-justified">
+            <li className="mx-2 nav-item">
+                <div className="nabvar-item nav-pills nabvar">
+                    <h5>Ingresar</h5>
+                </div>
+            </li>
+            <li className="mx-2 nav-item">
+                <div className="nabvar-item nav-pills nabvar">
+                    <h5>Registrarse</h5>
+                </div>
+            </li>
+        </ul>
     );
 }
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-sm nav position-absolute fixed-top">
-            <Item name="Portal UNS"/>
-            <Item name="Ingresar"/>
-            <Item name="Registrarse"/>
+            <div className="nabvar-item nav-pills nabvar">
+                <h5>Portal UNS</h5>
+            </div>
+            <div className="ml-auto">
+                <NavbarList isLoggedIn={true}/>
+            </div>
         </nav>
     );
 }
