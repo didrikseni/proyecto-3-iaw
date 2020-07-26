6761bed44acd081bb112e39915a8c3fe4638710e
didrikseni <iandidri@gmail.com>
2020-07-26 17:23:15 -0300

    fixed footbar

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -31,56 +31,54 @@ class Article extends React.Component {
       <>
         <Navbar />
         <div className="page-content mx-5">
-          <div className="content-flex">
-            <div id="title" className="custom-text">
-              <h2>{this.state ? this.state.article.title : 'Loading..'}</h2>
+          <div id="title" className="custom-text">
+            <h2>{this.state ? this.state.article.title : 'Loading..'}</h2>
+          </div>
+          <div className="custom-text flex-grow-1">
+            <div className="flex-grow-1">
+              <Editor
+                value={this.state ? this.state.article.content : ''}
+                apiKey="3yn19ck0mgv6qus3qkej8vrfp9x3q45am4ikvprcke9nzs7q"
+                init={{
+                  selector: 'textarea',
+                  toolbar: false,
+                  menubar: false,
+                  tinycomments_mode: 'embedded',
+                  tinycomments_author: 'Author name',
+                  readonly: 1,
+                  plugins: ['autoresize', 'preview', 'autolink'],
+                  branding: false,
+                }}
+              />
             </div>
-            <div className="custom-text flex-grow-1">
-              <div className="flex-grow-1">
-                <Editor
-                  value={this.state ? this.state.article.content : ''}
-                  apiKey="3yn19ck0mgv6qus3qkej8vrfp9x3q45am4ikvprcke9nzs7q"
-                  init={{
-                    selector: 'textarea',
-                    toolbar: false,
-                    menubar: false,
-                    tinycomments_mode: 'embedded',
-                    tinycomments_author: 'Author name',
-                    readonly: 1,
-                    plugins: ['autoresize', 'preview', 'autolink'],
-                    branding: false,
-                  }}
-                />
-              </div>
-              <br /> <br />
+            <br /> <br />
+          </div>
+          <div className="row custom-text">
+            <div className="ml-auto">
+              {this.state ? (
+                <h4>
+                  Autor:{' '}
+                  <Link to={'/profile/' + this.state.article.user_id} className="custom-text card-link">
+                    {this.state.article.author}
+                  </Link>
+                </h4>
+              ) : (
+                <h4> Loading.. </h4>
+              )}
             </div>
-            <div className="row custom-text">
-              <div className="ml-auto">
-                {this.state ? (
-                  <h4>
-                    Autor:{' '}
-                    <Link to={'/profile/' + this.state.article.user_id} className="custom-text card-link">
-                      {this.state.article.author}
-                    </Link>
-                  </h4>
-                ) : (
-                  <h4> Loading.. </h4>
-                )}
-              </div>
-            </div>
-            <div className="row custom-text mb-5">
-              <ul className="list-unstyled">
-                {this.state ? (
-                  this.state.article.tags.map((tag) => (
-                    <Link to={'/tags/' + tag.id} className="card-link custom-text m-2">
-                      {tag.name}
-                    </Link>
-                  ))
-                ) : (
-                  <li className="custom-text">Loading tags...</li>
-                )}
-              </ul>
-            </div>
+          </div>
+          <div className="row custom-text mb-5">
+            <ul className="list-unstyled">
+              {this.state ? (
+                this.state.article.tags.map((tag) => (
+                  <Link to={'/tags/' + tag.id} className="card-link custom-text m-2">
+                    {tag.name}
+                  </Link>
+                ))
+              ) : (
+                <li className="custom-text">Loading tags...</li>
+              )}
+            </ul>
           </div>
         </div>
         <Footer />
